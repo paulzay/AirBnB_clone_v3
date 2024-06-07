475d36d65c7c17251f099165e3483b029c2defc3
kimaita <johnnieqaym@gmail.com>
2024-06-07 19:15:13 +0300

    added 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,11 @@ def teardown(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return {"error": "Not found"}, 404
+
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", "5000"))
