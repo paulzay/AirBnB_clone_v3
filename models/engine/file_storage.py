10e13e5b02cddd9e9cf04f2eecd3449fd1b0a91b
kimaita <johnnieqaym@gmail.com>
2024-06-07 14:10:15 +0300

    fixes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,8 +12,15 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {
+    "Amenity": Amenity,
+    "BaseModel": BaseModel,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User,
+}
 
 
 class FileStorage:
@@ -45,13 +52,13 @@ class FileStorage:
         json_objects = {}
         for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, 'w') as f:
+        with open(self.__file_path, "w") as f:
             json.dump(json_objects, f)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as f:
+            with open(self.__file_path, "r") as f:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
@@ -61,7 +68,7 @@ class FileStorage:
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
+            key = obj.__class__.__name__ + "." + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
@@ -83,6 +90,7 @@ class FileStorage:
     def get(self, cls, id):
         """retrieve one object"""
         if cls is not None and id is not None:
-            key = cls + '.' + str(id)
+            cls_name = cls if isinstance(cls, str) else cls.__name__
+            key = cls_name + "." + id
             return self.__objects.get(key)
         return None
