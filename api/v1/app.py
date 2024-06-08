528467ca77335f3051c8c951072c027ad02b440a
kimaita <johnnieqaym@gmail.com>
2024-06-08 13:49:44 +0300

    rearrange

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,15 +3,17 @@
 starts a Flask web application
 """
 
+
+from api.v1.views import app_views
 from flask import Flask
 from flask_cors import CORS
 from models import storage
-from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
