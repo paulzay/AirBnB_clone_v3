6b29b382457ebe91add1b157542442c41123a270
kimaita <johnnieqaym@gmail.com>
2024-06-08 12:26:52 +0300

    added state methods

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,4 +5,5 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-import api.v1.views.index
+from api.v1.views.index import *
+from api.v1.views.states import *
