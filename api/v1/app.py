2913694a5720d4522774acc890fccb6cbe60f377
Paul Ogolla <paulotieno2@gmail.com>
2024-06-09 18:28:08 +0300

    redo

---
+++ api/v1/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+
+from api.v1.views import app_views
+from flask import Flask
+from flask_cors import CORS
+from models import storage
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """call storage.close"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """handle 404 errors"""
+    return {"error": "Not found"}, 404
+
+
+if __name__ == "__main__":
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", "5000"))
+    app.run(host, port, threaded=True)
