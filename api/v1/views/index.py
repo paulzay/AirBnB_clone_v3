f9646c39070315c33637a7d515c92aede94a5113
Paul Ogolla <paulotieno2@gmail.com>
2024-06-09 18:31:47 +0300

    update index

---
+++ api/v1/views/index.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""routing"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route("/status", strict_slashes=False)
+def status_route():
+    """return status OK"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats_route():
+    """Returns the number of objects by type"""
+
+    classes = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User,
+    }
+
+    obj_stats = {k: storage.count(v) for k, v in classes.items()}
+
+    return obj_stats
